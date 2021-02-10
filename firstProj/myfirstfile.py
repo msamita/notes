cece84f6da26b828189393b80aad29de762123db
amita <msamita@hotmail.com>
2021-02-10 13:11:22 -0500

    added python files

---
+++ firstProj/myfirstfile.py
@@ -0,0 +1,35 @@
+def add_number(num1, num2):
+    num = num1 + num2
+    print(num)
+
+add_number(5,45)
+print("hi"*10)
+print("*"* 7)
+text1 = "i M"
+text2 = "here"
+print (text1  + "  "+ text2)
+print(text1,text2)
+text = "hello Jack"
+print(text[:-7])
+text ="hello Jack"
+print(text[2:8])
+text="please convert me to upper case"
+print(text .upper())
+text_new=text.upper()
+print(text_new)
+print(text.count("e"))
+text="Replace Aadi with Arya"
+print(text.replace("Aadi","Arya"))
+print(len(text))
+text="123456"
+print(text.isalnum())
+print(text.isalpha())
+print(text.isdigit())
+print(text.isupper())
+number=1.55
+number_int=int(number)
+print(number_int)
+text = "5"
+text_int = int(text)
+print(type(text_int))
+
